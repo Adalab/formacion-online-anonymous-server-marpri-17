8fa8677f41ddbc6fe90eb1bb9ba6e658e8a3fd6c
marpri-17 <mar.pnieto@gmail.com>
2019-11-09 17:19:23 +0100

    prepare scss

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 'use strict';
 
-// Elements
 const buttonOpenMenu = document.querySelector(".nav-trigger");
 const buttonCloseMenu = document.querySelector(".nav-close");
+
 buttonCloseMenu.classList.add("js-hidden");
 buttonCloseMenu.innerHTML = "";
 buttonOpenMenu.innerHTML = "";
 
-//button.classList.add("displaybutton");
 const menuNav = document.querySelector(".nav");
 menuNav.classList.add("js-hidden")
 
 
 function toggleMenu() {
-    console.log("toggle menu")
     menuNav.classList.toggle("js-hidden");
     buttonCloseMenu.classList.toggle("js-hidden");
 }
