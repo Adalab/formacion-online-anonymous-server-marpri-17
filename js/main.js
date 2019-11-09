2c160e3fde0bda794e73327729e1adfd812d3117
marpri-17 <mar.pnieto@gmail.com>
2019-11-09 17:12:43 +0100

    first mobile layout with details and dinamic menu finish

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 'use strict';
 
+// Elements
+const buttonOpenMenu = document.querySelector(".nav-trigger");
+const buttonCloseMenu = document.querySelector(".nav-close");
+buttonCloseMenu.classList.add("js-hidden");
+buttonCloseMenu.innerHTML = "";
+buttonOpenMenu.innerHTML = "";
+
+//button.classList.add("displaybutton");
+const menuNav = document.querySelector(".nav");
+menuNav.classList.add("js-hidden")
+
+
+function toggleMenu() {
+    console.log("toggle menu")
+    menuNav.classList.toggle("js-hidden");
+    buttonCloseMenu.classList.toggle("js-hidden");
+}
+
+buttonOpenMenu.addEventListener("click", toggleMenu);
+buttonCloseMenu.addEventListener("click", toggleMenu)
